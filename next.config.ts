31c5340f5f4447cf68a316864af59eb088ad491a
Pexieftw <idir0204@gmail.com>
2025-04-08 21:37:56 +0100

    Config Change

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [{
-        loader: '@svgr/webpack',
-        options: {
-          svgoConfig: {
-            plugins: [
-              {
-                name: 'preset-default',
-                params: {
-                  overrides: {
-                    removeViewBox: false
-                  }
-                }
-              }
-            ]
-          }
-        }
-      }]
-    });
-    return config;
-  }
 };
 
 export default nextConfig;
